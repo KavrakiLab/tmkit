d3166fa64ab7566acc3594e2f39aacb618bfd717
Neil T. Dantam <ntd@rice.edu>
2015-08-07 18:15:30 -0500

    Grasp block from side

--- test/itmp-trash.lisp
+++ test/itmp-trash.lisp
@@ -23,8 +23,30 @@
 (defparameter *object-goal*
   (load-scene-file (rope *scene-directory* "labscene_goal.robray")))
 
+(defparameter *tf-grasp-top* (tf* (y-angle (* 1 pi)) (vec3* .00 .00 .10)))
+
+(defparameter *tf-grasp-back* (tf* (y-angle (* .5 pi)) (vec3* -.10 .00 .025)))
+
+(defparameter *tf-grasp-rel* *tf-grasp-back*)
+
+(defparameter *draw-scene*
+  (scene-graph *robot* *object-graph*
+               (scene-frame-fixed "block_a" "block_a_grasp" :tf *tf-grasp-back*)
+               ;(scene-frame-fixed "block_a" "block_a_grasp" :tf *tf-grasp-top*)
+               (robray::item-frame-marker "block_a_grasp" (robray::draw-subframe "block_a" "marker")
+                                          :length .15
+                                          :width .015
+                                          :options (robray::draw-options :no-shadow t :alpha .5
+                                                                         :visual t :collision nil))
+
+               (robray::item-frame-marker "right_endpoint" (robray::draw-subframe "right_endpoint" "marker")
+                                          :length .15
+                                          :width .015
+                                          :options (robray::draw-options :no-shadow t :alpha .5
+                                                                         :visual t :collision nil))))
+
 (robray::scene-graph-pov-frame
- (scene-graph *robot* *object-graph*)
+ *draw-scene*
  :configuration-map
  (alist-tree-map `(("right_s0" . ,(* .25 pi))
                    ("right_s1" . ,(* -0.25 pi))
@@ -40,6 +62,20 @@
                                   :options (render-options-medium))
  :output "robray.pov")
 
+
+
+
+
+
+
+(moveit-init (robray::urdf-resolve-file "package://baxter_description/urdf/baxter.urdf"))
+(defparameter *group* "right_arm")
+(defparameter *link* (container-group-endlink *moveit-container* *group*))
+(defparameter *q-all-start* (amino:make-vec (container-variable-count *moveit-container*)))
+(container-set-group *moveit-container* *group*)
+
+
+
 (time
  (progn
    (setq *plan*
@@ -49,23 +85,9 @@
                    :max-steps 1 :resolution .2))
    nil))
 
-
-
-
-
-(moveit-init (robray::urdf-resolve-file "package://baxter_description/urdf/baxter.urdf"))
-(defparameter *group* "right_arm")
-(defparameter *link* (container-group-endlink *moveit-container* *group*))
-(defparameter *q-all-start* (amino:make-vec (container-variable-count *moveit-contaigner*)))
-(container-set-group *moveit-container* *group*)
-
-
-(defparameter *tf-grasp-rel* (tf* (y-angle (* 1 pi)) (vec3* .00 .00 .10)))
-
 (render-group-itmp *plan-context* *group*
                      *plan*
                      :render-options  (render-options-default :use-collision nil
                                                               :options (render-options-fast))
-                     :scene-graph (scene-graph (plan-context-robot-graph *plan-context*)
-                                               *object-graph*)
+                     :scene-graph *draw-scene*
                      :frame-name "right_endpoint")
