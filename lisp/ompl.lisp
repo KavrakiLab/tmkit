6d042100e889d94bf3c1d745a4309b3cd36cd309
Neil T. Dantam <ntd@rice.edu>
2015-12-08 19:52:18 -0600

    Delete ROS and MoveIt interfaces

--- lisp/ompl.lisp
+++
@@ -1,8 +0,0 @@
-(in-package :tmsmt)
-
-
-(cffi:define-foreign-library libompl
-  (:unix "/usr/local/lib/libompl.so")
-  (t (:default "/usr/local/lib/libompl")))
-
-(cffi:use-foreign-library libompl)
--- lisp/ros/ros-loadlib.lisp
+++
@@ -1,9 +0,0 @@
-(in-package :tmsmt)
-
-(cffi:define-foreign-library libtmsmt
-    (:unix (:or #.(concatenate 'string (namestring (user-homedir-pathname))
-                               "ros_ws/src/tmsmt/devel/lib/libtmsmt.so")
-                "libtmsmt.so"))
-  (t (:default "libtmsmt")))
-
-(cffi:use-foreign-library libtmsmt)
