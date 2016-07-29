f5ad8f4a7443c93d25371487d523424815fc9bb2
Neil T. Dantam <ntd@rice.edu>
2016-07-29 14:33:40 -0500

    Generate manual with doxygen

--- include/tmsmt/tmplan.h
+++ include/tmsmt/tmplan.h
@@ -1,86 +1,197 @@
 #ifndef TMPLAN_H
 #define TMPLAN_H
 
+/**
+ * @file tmplan.h
+ * @brief Data structure for task-motion plans
+ */
+
+
+/**
+ * Types of operators in a task-motion plan.
+ */
 enum tmplan_op_type {
-    TMPLAN_OP_ACTION,
-    TMPLAN_OP_MOTION_PLAN,
-    TMPLAN_OP_REPARENT
+    TMPLAN_OP_ACTION,       ///< A task action
+    TMPLAN_OP_MOTION_PLAN,  ///< A motion plan
+    TMPLAN_OP_REPARENT      ///< A scene-graph reparenting
 };
 
+
+/**
+ * @struct tmplan
+ *
+ * Opaque structure for a task-motion plan
+ */
 struct tmplan;
 
+/**
+ * Create a new task-motion plan allocated out of the given region.
+ *
+ *
+ * To deallocate the task-motion plan, pop it from the region using
+ * aa_mem_region_pop().
+ */
 AA_API struct tmplan *
 tmplan_create (struct aa_mem_region *reg);
 
+/**
+ * Finalize the last added operator.
+ *
+ * This function is called internally by the plan file parser.
+ */
 AA_API void
 tmplan_finish_op(struct tmplan * tmp );
 
+/**
+ * Add a new operator to the plan.
+ *
+ * @pre The previously added operator (if any) must be finalized
+ *      before this operator is allocated from the memory region.
+ *
+ * This function is called internally by the plan file parser.
+ */
 AA_API void
 tmplan_add_op(struct tmplan * tmp, void *op);
 
+/**
+ * Add a task operator to the plan.
+ */
 AA_API void
 tmplan_add_action(struct tmplan * tmp);
 
+/**
+ * Add a motion plan to the plan.
+ */
 AA_API void
 tmplan_add_motion_plan(struct tmplan * tmp);
 
+/**
+ * Add a reparent operator to the plan.
+ */
 AA_API void
 tmplan_add_reparent(struct tmplan * tmp);
 
+/**
+ * @struct tmplan_op
+ *
+ * Opaque structure for a task operator.
+ */
 
 struct tmplan_op;
 
+/**
+ * Return a pointer to the last added operator.
+ *
+ * This function is used internally by the plan file parser.
+ */
 AA_API struct tmplan_op *
 tmplan_last_op(struct tmplan * tmp);
 
+/**
+ * Return the type of the operator.
+ */
 AA_API enum tmplan_op_type
 tmplan_op_type( struct tmplan_op *op );
 
+/**
+ * @struct tmplan_op_action
+ *
+ * Opaque type for a task action.
+ */
 struct tmplan_op_action;
 
+/**
+ * Set the value of the task action, represented as a string.
+ */
 AA_API void
 tmplan_op_action_set (struct tmplan_op_action *op, const char *value );
 
+/**
+ * Get the value of the task action, represented as a string.
+ */
 AA_API const char *
 tmplan_op_action_get (const struct tmplan_op_action *op );
 
+/**
+ * @struct tmplan_op_motion_plan
+ *
+ * Opaque type for a motion plan.
+ */
 struct tmplan_op_motion_plan;
 
+/**
+ * Add a new configuration variable to the motion plan.
+ */
 AA_API void
 tmplan_op_motion_plan_add_var (struct tmplan_op_motion_plan *op, const char *name);
 
+/**
+ * Apply function to each configuration variable name in the motion plan.
+ */
 AA_API void
 tmplan_op_motion_plan_map_var (struct tmplan_op_motion_plan *op,
                                void (*function)(void *cx, const char *name),
                                void *cx);
 
+/**
+ * Preprate to add points to the motion plan.
+ */
 AA_API void
 tmplan_op_motion_plan_path_start (struct tmplan_op_motion_plan *op );
 
+/**
+ * Add the next waypoint configuration value to the motion plan.
+ */
 AA_API void
 tmplan_op_motion_plan_path_add (struct tmplan_op_motion_plan *op, double x);
 
+/**
+ * Finalize the motion plan path.
+ */
 AA_API void
 tmplan_op_motion_plan_path_finish (struct tmplan_op_motion_plan *op );
 
+/**
+ * @struct tmplan_op_motion_plan
+ *
+ * Opaque type for a reparent operation.
+ */
 struct tmplan_op_reparent;
 
+/**
+ * Set the frame being reparented.
+ */
 AA_API void
 tmplan_op_reparent_set_frame (struct tmplan_op_reparent *op, const char *frame);
 
+/**
+ * Set the new parent.
+ */
 AA_API void
 tmplan_op_reparent_set_new_parent (struct tmplan_op_reparent *op, const char *frame);
 
+/**
+ * Get the frame being reparented.
+ */
 AA_API const char*
 tmplan_op_reparent_get_frame (struct tmplan_op_reparent *op);
 
+/**
+ * Get the new parent.
+ */
 AA_API const char*
 tmplan_op_reparent_get_new_parent (struct tmplan_op_reparent *op);
 
+/**
+ * Parse a plan file, allocation out of the given region.
+ */
 AA_API struct tmplan*
 tmplan_parse (FILE *in, struct aa_mem_region *reg);
 
 
+/**
+ * Write a tmplan to `out'.
+ */
 AA_API int
 tmplan_write (const struct tmplan *tmp, FILE *out );
 
