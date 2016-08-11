a6bbb83d038d414f82edaee34b557965efc75f07
Neil T. Dantam <ntd@rice.edu>
2016-08-10 20:05:43 -0500

    Initial domain script for standard blocksworld

---
+++ demo/domains/blocksworld/tm-blocks.py
@@ -0,0 +1,203 @@
+#########################
+### Domain Parameters ###
+#########################
+
+# Table grid resolution
+RESOLUTION=0.1
+
+# End-Effector Frame
+FRAME = "end_effector_grasp"
+
+# Object placement tolerance
+EPSILON = 1e-4
+
+
+
+########################
+### Imported Modules ###
+########################
+
+# Import our planning packages
+import aminopy as aa
+import tmsmtpy as tm
+
+import TMSMT
+
+# Import python math package
+from math import pi
+
+# The Common Lisp runtime is also available
+import CL as cl
+
+
+##########################
+## Scene State Function ##
+##########################
+
+def scene_state_linear(scene, configuration):
+    '''Map the scene graph `scene' to a task state expression'''
+
+    ## terms in the expression
+    conjunction = []
+
+    def handle_moveable(frame):
+        name = frame['name']
+        parent_name = frame['parent']
+
+        try:
+            # If parent frame is a placement surface, position is the
+            # appropriate grid cell on the surface.
+            parent_frame = scene[parent_name]
+            if aa.frame_isa(parent_frame, "surface"):
+                trans = aa.translation( aa.frame_fixed_tf(frame) )
+                i = int(round( trans[0] / RESOLUTION))
+                j = int(round( trans[1] / RESOLUTION))
+                position = tm.mangle(parent_name,i,j)
+                conjunction.append(["ONTABLE", name, position])
+            else:
+                conjunction.append(["ON", name, parent_name])
+        except NameError:
+                conjunction.append(["ON", name, parent_name])
+
+
+    moveable_frames = tm.collect_frame_type(scene,"moveable")
+    map(handle_moveable, moveable_frames)
+
+    conjunction.append(["HANDEMPTY"])
+
+    # TODO: clear locations
+
+    return conjunction
+
+
+############################
+## Scene Objects Function ##
+############################
+
+def scene_objects_linear(scene):
+    '''Return the PDDL objects for `scene'.'''
+    obj = []
+
+    def type_names(thing):
+        return [ f['name']
+                 for f in
+                 tm.collect_frame_type(scene,thing) ]
+
+    # Moveable objects are blocks
+    moveable = type_names("moveable")
+    moveable.insert(0, "BLOCK")
+
+    # Draw grid on surfaces
+    locations = ['LOCATION']
+    def add_loc(name,i,j):
+        locations.append(tm.mangle(name,i,j))
+
+    for frame in tm.collect_frame_type(scene,"surface"):
+        name = frame['name']
+        for geom in frame['geometry']:
+            shape = geom['shape']
+            if aa.shape_is_box(shape):
+                d = shape['dimension']
+                x_max = d[0] / 2
+                y_max = d[1] / 2
+                x = 0
+                i = 0
+                while x <= x_max:
+                    y = 0
+                    j = 0
+                    while y <= y_max:
+                        add_loc(name,i,j)
+                        if i > 0: add_loc(name,-i,j)
+                        if j > 0: add_loc(name,i,-j)
+                        if i > 0 and j > 0: add_loc(name,-i,-j)
+                        y += RESOLUTION
+                        j+=1
+                    x +=  RESOLUTION
+                    i+=1
+    return [moveable, locations]
+
+############################
+### Operator Definitions ###
+############################
+
+
+def motion_plan(op, frame, goal):
+    scene = op['final_scene']
+    sub_scenegraph = aa.scene_chain(scene, "", frame)
+    mp = tm.op_motion( op, sub_scenegraph, goal )
+    if False == mp: raise tm.PlanningFailure()
+    else: return mp
+
+# def pick(op, obj):
+#     mp = motion_plan(op, FRAME, tm.op_tf_abs(op,obj))
+#     return tm.op_reparent(mp, FRAME, obj)
+
+def place_tf(op, obj, dst_frame, g_tf_o ):
+    mp =  motion_plan(op, obj, g_tf_o)
+    return tm.op_reparent(mp, dst_frame, obj)
+
+def place_height(scene,name):
+    g = scene[name]['collision']
+    s = g[0]['shape']
+    if aa.shape_is_box(s):
+        return s['dimension'][2] / 2
+
+# def place(op, obj, dst, i, j):
+#     scene = op['final_scene']
+#     x = i*RESOLUTION
+#     y = j*RESOLUTION
+#     z = place_height(scene,obj) + place_height(scene,dst) + EPSILON
+#     d_tf_o = aa.tf2( 1, [x,y,z] )
+#     g_tf_d = tm.op_tf_abs(op,dst)
+#     g_tf_o = aa.mul(g_tf_d, d_tf_o );
+#     return place_tf(op, obj, dst, g_tf_o)
+
+# def stack(op, obj, dst ):
+#     scene = op['final_scene']
+#     config = op['final_config']
+#     g_tf_d = tm.op_tf_abs(op,dst)
+#     d_tf_o = aa.tf2(1, [0,0, place_height(scene,obj) + place_height(scene,dst) + EPSILON])
+#     g_tf_o = aa.mul(g_tf_d, d_tf_o)
+#     return place_tf(op, obj, dst, g_tf_o)
+
+
+
+def op_pick_up(scene, config, op):
+    (a, obj, loc) = op
+    nop = tm.op_nop(scene,config)
+    mp = motion_plan(nop, FRAME, tm.op_tf_abs(nop,obj))
+    return tm.op_reparent(mp, FRAME, obj)
+
+def op_put_down(scene, config, op):
+    (a, obj, loc) = op
+    nop = tm.op_nop(scene,config)
+    x = i*RESOLUTION
+    y = j*RESOLUTION
+    z = place_height(scene,obj) + place_height(scene,dst) + EPSILON
+    d_tf_o = aa.tf2( 1, [x,y,z] )
+    g_tf_d = tm.op_tf_abs(nop,dst)
+    g_tf_o = aa.mul(g_tf_d, d_tf_o );
+    return place_tf(nop, obj, dst, g_tf_o)
+
+def op_stack(scene, config, op):
+    (act,obj,dst) = op
+    nop = tm.op_nop(scene,config)
+    g_tf_d = tm.op_tf_abs(nop,dst)
+    d_tf_o = aa.tf2(1, [0,0, place_height(scene,obj) + place_height(scene,dst) + EPSILON])
+    g_tf_o = aa.mul(g_tf_d, d_tf_o)
+    return place_tf(nop, obj, dst, g_tf_o)
+
+def op_unstack(scene, config, op):
+    return op_pick_up(scene,config,op)
+
+
+##########################
+### Register functions ###
+##########################
+
+tm.bind_scene_state(scene_state_linear)
+tm.bind_scene_objects(scene_objects_linear)
+tm.bind_refine_operator(op_pick_up, "PICK-UP")
+tm.bind_refine_operator(op_put_down, "PUT-DOWN")
+tm.bind_refine_operator(op_stack, "STACK")
+tm.bind_refine_operator(op_unstack, "UNSTACK")
---
+++ demo/run-baxter-sussman.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+## Compute a task-motion plan to solve the sussman anomaly with a
+## Rethink Baxter robot.
+
+
+export ROS_PACKAGE_PATH=/opt/ros/indigo/share
+export PATH="..:$PATH"
+
+tmsmt -s 'package://baxter_description/urdf/baxter.urdf' \
+      -s baxter-sussman/sussman-0.robray \
+      -s baxter-sussman/allowed-collision.robray \
+      -g baxter-sussman/sussman-1.robray  \
+      -d domains/blocksworld/tm-blocks.pddl \
+      -l domains/blocksworld/tm-blocks.py \
+      -o baxter-sussman-linear.tmp \
+      --gui \
+      --verbose
