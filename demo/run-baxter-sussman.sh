6efb1a75be73a865f3b3acb17d82232d84ae8e94
Neil T. Dantam <ntd@rice.edu>
2016-08-12 18:48:49 -0500

    Use non-flagged args for planner command input

--- demo/run-baxter-sussman.sh
+++ demo/run-baxter-sussman.sh
@@ -3,16 +3,19 @@
 ## Compute a task-motion plan to solve the sussman anomaly with a
 ## Rethink Baxter robot.
 
+# Maybe set ROS_PACKAGE_PATH
+if [ -z "$ROS_PACKAGE_PATH" ]; then
+   export ROS_PACKAGE_PATH=/opt/ros/indigo/share
+fi
 
-export ROS_PACKAGE_PATH=/opt/ros/indigo/share
 export PATH="..:$PATH"
 
-tmsmt -s 'package://baxter_description/urdf/baxter.urdf' \
-      -s baxter-sussman/sussman-0.robray \
-      -s baxter-sussman/allowed-collision.robray \
+tmsmt package://baxter_description/urdf/baxter.urdf \
+      baxter-sussman/sussman-0.robray \
+      baxter-sussman/allowed-collision.robray \
+      domains/blocksworld/tm-blocks.pddl \
+      domains/blocksworld/tm-blocks.py \
       -g baxter-sussman/sussman-1.robray  \
-      -d domains/blocksworld/tm-blocks.pddl \
-      -l domains/blocksworld/tm-blocks.py \
       -o baxter-sussman.tmp \
       --gui \
       --write-facts=baxter-sussman.pddl \
