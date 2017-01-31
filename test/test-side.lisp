aed4d3fac14281e7ec4a6fa667fd7e4b5fd09290
Neil T. Dantam <ntd@rice.edu>
2017-01-31 15:52:29 -0600

    Use side grasp in test script

--- test/test-side.lisp
+++ test/test-side.lisp
@@ -22,7 +22,7 @@
 
 
 (defparameter *table-dim* (vec3* .5 .5 .01))
-(defparameter *box-dim* .060)
+(defparameter *box-dim* .080)
 (defparameter *resolution* .10)
 (defparameter *z* (/ (+ .02 (vec-z *table-dim*) *box-dim*)
                      2))
@@ -54,6 +54,9 @@
   (scene-graph (scene-frame-fixed "front_table"  "block-0"
                                   :tf (tf* nil (vec3* (* 1 *resolution*) 0 *z*))
                                   :geometry *geometry*)
+               (scene-frame-fixed "front_table"  "block-1"
+                                  :tf (tf* nil (vec3* (* 1 *resolution*) (* -1 *resolution*) *z*))
+                                  :geometry *geometry*)
   ))
 
 
@@ -63,26 +66,44 @@
                                   :geometry *geometry*)
   ))
 
+(defparameter *tf-top* (tf* (quaternion* 0 1 0 0)
+                            (vec3* 0 0 .075)))
+
+(defparameter *tf-side* (inverse (tf* (g* (quaternion* 0 1 0 0)
+                                          (euler-rpy* (* -.4 pi ) 0 0))
+                                      (vec3* 0 -.06 .02))))
+
+
 (defparameter *scenegraph*
   (scene-graph *baxter* *sg-table* *sg-block*
                (scene-frame-fixed "right_endpoint" "end_effector_grasp"
-                                  :tf (tf* (quaternion* 0 1 0 0)
-                                           (vec3* 0 0 .075)))))
-(robray::win-set-scene-graph *scenegraph*)
+                                  :tf *tf-side*)))
+(robray::win-set-scene-graph *scenegraph* :end-effector "right_endpoint")
 (robray::win-run)
 
 
 
-(tmp-driver :start-scene (scene-graph *scenegraph*
-                                      (demo-file :directory "baxter-sussman"
-                                                 :name "allowed-collision"
-                                                 :type "robray"))
-            :goal-scene (scene-graph *sg-table* *sg-block-1*)
-            :gui t
-            :pddl (demo-file :directory '("domains" "linear-blocksworld")
-                             :name "linear-blocksworld"
-                             :type "pddl")
-            :output "/tmp/tmkit-test.tmp"
-            :scripts (demo-file :directory '("domains" "linear-blocksworld")
-                                :name "linear-blocksworld"
-                                :type "py"))
+;; (tmp-driver :start-scene (scene-graph *scenegraph*
+;;                                       (demo-file :directory "baxter-sussman"
+;;                                                  :name "allowed-collision"
+;;                                                  :type "robray"))
+;;             :goal-scene (scene-graph *sg-table* *sg-block-1*)
+;;             :gui t
+;;             :pddl (demo-file :directory '("domains" "linear-blocksworld")
+;;                              :name "linear-blocksworld"
+;;                              :type "pddl")
+;;             :output "/tmp/tmkit-test.tmp"
+;;             :scripts (demo-file :directory '("domains" "linear-blocksworld")
+;;                                 :name "linear-blocksworld"
+;;                                 :type "py"))
+
+
+;; (let ((q (robray::scene-graph-ik *scenegraph*
+;;                                  :frame "right_endpoint"
+;;                                  :parent "block-1"
+;;                                  ;:tf (inverse *tf-top*)
+;;                                  :tf (inverse *tf-side*)
+;;                                  )))
+;;   (print q)
+;;   (if q
+;;     (robray::win-set-config q)))
