7b75ef21563a181c2f11d6f2b243caa451b05fdc
Neil T. Dantam <ntd@rice.edu>
2017-03-02 19:26:16 -0600

    Use core file to build docs

--- doc.lisp
+++ doc.lisp
@@ -1,7 +1,1 @@
-(let ((top-srcdir (concatenate 'string
-                               (uiop/os:getenv "top_srcdir")
-                               "/")))
-  (load (merge-pathnames "load.lisp" top-srcdir)))
-
-(ql:quickload :ntdoc)
 (ntdoc::markdown "tmsmtpy" :system :tmsmt :target "py-api.md" :title "Domain Semantics API (Python)")
